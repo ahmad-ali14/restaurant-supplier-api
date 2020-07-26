29bc7ab3638640df298a07ed63702fbc00213e78
Ahmad Ali <aallii300300@gmail.com>
2020-07-26 01:35:50 +0100

    connecting to mongo

--- utils/dbHandler/dbHandler.go
+++ utils/dbHandler/dbHandler.go
@@ -1,5 +1,40 @@
 package dbHandler
 
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"restaurant-supplier-api/config"
+	"time"
+)
+
+func CreateConnection() *mongo.Client {
+	// mongo conncetion
+	var mongoURI = config.GetMongoUrl()
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+
+	//collection := client.Database("portfolio").Collection("restaurants")
+
+	// Ping MongoDB
+	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	if err = client.Ping(ctx, readpref.Primary()); err != nil {
+		fmt.Println("could not ping to mongo db service: %v\n", err)
+		//return "error, no connection"
+	}
+
+	// fmt.Printf("ctx is %s", ctx)
+
+	return client
+
+}
+
 func AddItemToDb() string {
 	return "Added"
 }
