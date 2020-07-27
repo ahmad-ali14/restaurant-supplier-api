9109e5f12ff9279610628980bdf954244cbb68ac
Ahmad Ali <aallii300300@gmail.com>
2020-07-27 12:24:24 +0100

    supplier model

---
+++ models/supplier/supplier.go
@@ -0,0 +1,28 @@
+package supplier
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Supplier struct {
+	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Email string             `bson:"email,omitempty" json:"email,omitempty"`
+
+	Password string `bson:"password,omitempty" json:"password,omitempty"`
+
+	Name string `bson:"name,omitempty" json:"name,omitempty"`
+
+	Address string `bson:"address,omitempty" json:"address,omitempty"`
+
+	Phone string `bson:"phone,omitempty" json:"phone,omitempty"`
+
+	Role string `bson:"role,omitempty" json:"role,omitempty"`
+
+	Products []*Product `bson:"products,omitempty" json:"products,omitempty"`
+}
+
+type Product struct {
+	Name string `bson:"productName,omitempty" json:"productName,omitempty"`
+
+	Price string `bson:"productPrice,omitempty" json:"productPrice,omitempty"`
+}
