f0dab1ecdc45da1527517bfd790e1ff1b577b74c
Ahmad Ali <aallii300300@gmail.com>
2020-07-27 12:24:12 +0100

    user model

---
+++ models/user/user.go
@@ -0,0 +1,13 @@
+package user
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Email    string             `bson:"emial,omitempty" json:"emial,omitempty"`
+	Password string             `bson:"password,omitempty" json:"password,omitempty"`
+	Role     string             `bson:"role,omitempty" json:"role,omitempty"`
+	UserId   primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+}
